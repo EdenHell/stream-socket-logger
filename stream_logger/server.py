fc36f6b54d56ece46845c5a38076fe8668ccdda5
Frost Sigh <frostsigh@gmail.com>
2018-09-10 16:23:40 +0800

    fix bug

--- stream_logger/server.py
+++ stream_logger/server.py
@@ -62,3 +62,20 @@ def run_server(server_address):
             server.server_close()
             raise
         server.serve_forever(0.1)
+
+
+if __name__ == '__main__':
+    import json
+    import argparse
+    import logging.config
+
+    parser = argparse.ArgumentParser(description='stream logger server')
+    parser.add_argument('host', type=str, nargs=1, help='Connect to host.')
+    parser.add_argument('-p', '--port', type=int, nargs=1, help='Port number.')
+    parser.add_argument('-c', '--config', type=str, nargs=1, help='Config file path')
+    args = parser.parse_args()
+
+    with open(args.config) as f:
+        config = json.load(f)
+    logging.config.dictConfig(config)
+    run_server(args.host if args.port is None else (args.host, args.port))
